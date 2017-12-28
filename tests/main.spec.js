759a6f1804566c6617071fd1ec65e59ca89e3d78
Fabrício Silva <fabriciofmsilva@gmail.com>
2017-12-28 18:56:30 -0200

    Hooks na Prática

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,40 +1,41 @@
 describe('Main', function() {
 
+  var arr;
+
   // roda uma vez, antes do bloco
   before(function() {
-    console.log('before');
+
   });
 
   // roda uma vez, depois do bloco
   after(function() {
-    console.log('after');
+
   });
 
   // roda todas as vezes, antes de CADA bloco
   beforeEach(function() {
-    console.log('beforeEach');
+    arr = [1, 2, 3];
   });
 
   // roda todas as vezes, depois de CADA bloco
   afterEach(function() {
-    console.log('afterEach');
+
   });
 
-  it('test 1', function() {
-    console.log('test 1');
+  it('should have a size of 4 when push another value to the array', function() {
+    arr.push(4);
+    console.log(arr.length); // 4
   });
 
-  it('test 2', function() {
-    console.log('test 2');
+  it('should have a size of 2 when pop a value from array', function() {
+    var arr = [1, 2, 3];
+    arr.pop();
+    console.log(arr.length); // 2
+  });
+
+  it('should remove the value 3 when use pop in the array', function() {
+    var arr = [1, 2, 3];
+    console.log(arr.pop() === 3) // true
   });
 
 });
-
-// before
-// beforeEach
-// test 1
-// afterEach
-// beforeEach
-// test 2
-// afterEach
-// after
