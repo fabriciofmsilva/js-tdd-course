d50df505f0b28fa8268ce7cbed30eb6d70bf426a
Fabrício Silva <fabriciofmsilva@gmail.com>
2017-12-28 18:33:28 -0200

    Funcionamento do Mocha - describe, context, it...

---
+++ tests/main.spec.js
@@ -0,0 +1,19 @@
+describe('Main', function() {
+  describe('Method A', function() {
+    context('Case 1', function() {
+      it('should happen blablabla', function() {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', function() {
+
+    });
+  });
+
+  describe('Method B', function() {
+
+  });
+});
