e67ce365e6d279b5f8e16f3212cec65f6085b13c
Fabrício Silva <fabriciofmsilva@gmail.com>
2017-12-28 19:47:50 -0200

    Criando um FizzBuzz com TDD

--- tests/calc.spec.js
+++ tests/calc.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { sum, sub, mult, div } from '../src/calc';
 
-describe('Calc', () => {
+describe.skip('Calc', () => {
 
   // smoke test
   describe('smoke tests', () => {
---
+++ tests/fizzbuzz.spec.js
@@ -0,0 +1,38 @@
+/*
+
+Desafio FizzBuzz
+
+Escreva uma lib que receba um número e:
+
+Se o número for divisível por 3, no lugar do número escreva 'Fizz' - X
+Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
+Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - X
+Se não for múltiplo de nada, retorna o número
+
+*/
+
+import { expect } from 'chai';
+import FizzBuzz from '../src/fizzbuzz';
+
+describe('FizzBuzz', () => {
+
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  });
+
+  it('shoud return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
+  });
+
+  it ('should return `FizzBuzz` qhen multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+    expect(FizzBuzz(30)).to.be.equal('FizzBuzz');
+  });
+
+  it ('should return the number when non-multiple', () => {
+    expect(FizzBuzz(7)).to.be.equal(7);
+  });
+
+});
