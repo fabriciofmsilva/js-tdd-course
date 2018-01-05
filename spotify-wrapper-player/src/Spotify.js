43ed15765742e9ea62774c93ef52e90d3be7ca48
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-05 21:06:59 -0200

    Adicionando o AlbumList no Layout

--- spotify-wrapper-player/src/Spotify.js
+++ spotify-wrapper-player/src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'BLA'
+  token: 'BQD988zDI22T8b-int2zKLeYpZP3-ZnS6Kthm5PBj05agMCup7csS4gGkvLkqBnf9vbd0B-R8ehG_WnH8mwJizHPuEbR0lAnOqTc79ced6x219KocCqtPvys0ECpi75oBATa-dnGDa3CTv9U7tOD'
 });
 
 export default spotify;
