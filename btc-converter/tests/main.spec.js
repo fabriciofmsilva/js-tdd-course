a87fac0b54f5ad3e099d8b4d849c9fc6a8d9de0a
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-07 18:20:00 -0200

    Definindo Options como parâmetros

--- btc-converter/tests/main.spec.js
+++ btc-converter/tests/main.spec.js
@@ -20,4 +20,20 @@ describe('Main CLI', () => {
       done();
     });
   });
+
+  it('should return the currency when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('--currency')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return the amount when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if (err) throw err;
+      expect(stdout.includes('--amount')).to.be.true;
+      done();
+    });
+  });
 });
