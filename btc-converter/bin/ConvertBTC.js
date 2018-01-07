80f2589dba7f9037c2e11f274a62d5ed3be14ff9
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-07 19:02:09 -0200

    Adicionando um Loader

--- btc-converter/bin/ConvertBTC.js
+++ btc-converter/bin/ConvertBTC.js
@@ -2,15 +2,23 @@
 
 var chalk = require('chalk');
 var request = require('request');
+var ora = require('ora');
+
+var spinner = ora({
+  text: 'Retriving Biticoin data...',
+  color: 'yellow'
+});
 
 function convertBTC() {
   var currency = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'USD';
   var amount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
 
   var url = 'https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=' + currency + '&amount=' + amount;
+  spinner.start();
 
   request(url, function (error, response, body) {
     var apiResponse = void 0;
+    spinner.stop();
 
     try {
       apiResponse = JSON.parse(body);
