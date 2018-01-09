da0249ac7469e96f8ab0fe9dbc1e3cf0118869ab
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-08 23:35:46 -0200

    Adicionando título

--- react-full-header/stories/Main.js
+++ react-full-header/stories/Main.js
@@ -2,48 +2,48 @@ import React from 'react';
 import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('React Full Header', module)
-    .add('with title', () => {
+storiesOf('<FullHeader />', module)
+    .add('with title', () => (
         <FullHeader title="TDD" />
-    })
-    .add('with title and subtitle', () => {
+    ))
+    .add('with title and subtitle', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática."
         />
-    })
-    .add('with title, subtitle and bgColor', () => {
+    ))
+    .add('with title, subtitle and bgColor', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática."
-            bgColor="#3299bb"
+            bgColor="#3299BB"
         />
-    })
-    .add('with title, subtitle, bgColor and textColor', () => {
+    ))
+    .add('with title, subtitle, bgColor and textColor', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática."
-            bgColor="#3299bb"
-            textColor="#ff9900"
+            bgColor="#3299BB"
+            textColor="#FF9900"
         />
-    })
-    .add('with title, subtitle, bgColor, textColor and font', () => {
+    ))
+    .add('with title, subtitle, bgColor, textColor and font', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática."
-            bgColor="#3299bb"
-            textColor="#ff9900"
+            bgColor="#96060C"
+            textColor="#FF9900"
             font="cursive"
         />
-    })
-    .add('with title, subtitle and bgImage', () => {
+    ))
+    .add('with title, subtitle, bgImg', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática."
-            bgImage="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
+            bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"
         />
-    })
-    .add('with title, subtitle and video', () => {
+    ))
+    .add('with title, subtitle and video', () => (
         <FullHeader
             title="TDD"
             subtitle="Curso de JS com TDD na prática."
@@ -51,4 +51,4 @@ storiesOf('React Full Header', module)
             textColor="#3299BB"
             video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"
         />
-    });
+    ));
--- react-full-header/storybook/config.js
+++ react-full-header/storybook/config.js
@@ -1,7 +1,4 @@
 import { configure, addDecorator } from '@storybook/react';
-import GithubCorner from '@personare/react-storybook-decorator-github-corner';
-
-addDecorator(GithubCorner);
 
 import '../css/main.css';
 
